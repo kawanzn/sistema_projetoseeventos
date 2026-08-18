ee3a10262c33b85f0b1010504964a075adbe9fb2
Kawan Lopes Mangia <kawanlopesmangia123@gmail.com>
2026-08-18 12:12:18 -0300

    adiciona card de eventos ativos no dashboard

--- sistema-eventos/src/components/Dashboard.jsx
+++ sistema-eventos/src/components/Dashboard.jsx
@@ -8,12 +8,29 @@ function Dashboard() {
       {/* Título da página */}
       <h1>Dashboard</h1>
 
-      {/* Pequena descrição */}
+      {/* Pequena descrição da página */}
       <p>Visão geral dos projetos e eventos do setor.</p>
+
+
+      {/* Área que vai armazenar os cards do Dashboard */}
+      <div className="cards">
+
+        {/* Primeiro card: mostra a quantidade de eventos ativos */}
+        <div className="card">
+
+          {/* Nome da informação */}
+          <h3>Eventos Ativos</h3>
+
+          {/* Quantidade de eventos - valor temporário */}
+          <span>4</span>
+
+        </div>
+
+      </div>
 
     </main>
   )
 }
 
-// Permite utilizar o Dashboard em outros arquivos
+// Exporta o componente para poder utilizá-lo em outros arquivos
 export default Dashboard
